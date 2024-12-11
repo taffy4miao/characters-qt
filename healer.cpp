237f7041b60e6dd972df529c5482eab2b5468e72
taffy <taffy4miao@gmail.com>
2024-12-11 17:29:19 +0800

    框架

--- healer.cpp
+++ healer.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "healer.h"
+
+void healer::levelUp(int targetLevel) {
+
+}
+
+void healer::displayInfo() {
+
+}
+
+int healer::getType() {
+    return healerNum;
+}
+
+healer::healer(std::string name, int hp, int healValue, std::string skillName, int skillValue) {
+    this->name = std::move(name);
+    this->hp = hp;
+    this->healValue = healValue;
+    this->num = currentNum++;
+    this->level = 1;
+    skillHeal s(std::move(skillName),skillValue);
+    this->skill = s;
+}
+
+healer::skillHeal::skillHeal(std::string name, int value) {
+
+}
+
+void healer::skillHeal::skillLevelUp(int targetLevel) {
+
+}
--- healer.h
+++ healer.h
@@ -8,8 +8,23 @@
 #include "character.h"
 
 class healer : public character{
-protected:
+private:
     int healValue;
+    class skillHeal : public skill{
+    private:
+        int healValue;
+    public:
+        skillHeal(){
+            throw std::runtime_error("不应使用默认构造函数");
+        }
+        skillHeal(std::string name,int value);
+        void skillLevelUp(int targetLevel) override;
+    };
+public:
+    healer(std::string name,int hp,int healValue,std::string skillName,int skillValue);
+    void levelUp(int targetLevel) override;
+    void displayInfo() override;
+    int getType() override;
 };
 
 #endif //CHARACTERS_QT_HEALER_H
